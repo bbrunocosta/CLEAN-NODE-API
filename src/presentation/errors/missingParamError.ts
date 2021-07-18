c79251087c48764e6982475bfc5ece3ada2d81ea
Bruno Costa <b.brunocosta@outlook.com>
2021-07-18 19:48:26 -0300

    feat: add custom error

---
+++ src/presentation/errors/missingParamError.ts
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * Generic feed error
+ * @extends Error
+ * @classdesc Wraps feed errors allowing specification of the feed that errored.
+ * @property {string} name name of the error
+ * @property {string} message Standardized error message
+ * @property {string} paramName the field that is missing on the request
+ */
+// * @property {string} feed Feed URL producing the error
+export class MissingParamError extends Error {
+  /**
+   * Create a Feed error
+   * @param {string} message error message
+   * @param {string} type    Type of error, provides Error#name
+   * @param {string} paramName    Type of error, provides Error#name
+   */
+  // @param {string} feed    Feed url that originated the error
+  constructor (paramName: string) {
+    super()
+    /**
+     * Type of error
+     * @type {string}
+     */
+    this.name = 'MissingParamError'
+    /**
+     * Param name causing the error
+     * @type {string}
+     */
+    this.message = `The param ${paramName} is missing`
+  }
+
+  toString (): string {
+    return `${this.name} : ${this.message}` // \n${this.feed}
+  }
+}
