dba050040d40ab494145b911470f72883aa94e3d
Bruno Costa <b.brunocosta@outlook.com>
2021-07-19 14:04:41 -0300

    test: should call addAccount.add with correct values

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -1,15 +1,30 @@
 import { EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { SignUpController } from './signUp.controller'
+import { AddAccount, AddAccountObj } from '../../domain/useCases/addAccount.usecase'
+import { Account } from '../../domain/models/account'
 
 describe('SignUp Controller', () => {
+  class AddAccountStub implements AddAccount {
+    add (account: AddAccountObj): Account {
+      const fakeAccount = {
+        id: 'valid id',
+        name: 'valid name',
+        email: 'valid email',
+        password: 'valid password',
+        passwordConfirmation: 'valid password'
+      }
+      return fakeAccount
+    }
+  }
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       return true
     }
   }
   const emailValidatorStub = new EmailValidatorStub()
-  const sut = new SignUpController(emailValidatorStub)
+  const addAccountStub = new AddAccountStub()
+  const sut = new SignUpController(emailValidatorStub, addAccountStub)
   test('Should return 400 if no name is provided', () => {
     const httpRequest = {
       body: {
@@ -111,5 +126,18 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.status).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+  test('Should call addAccount with correct values', () => {
+    const addAccountSpy = jest.spyOn(addAccountStub, 'add')
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any email',
+        password: 'any password',
+        passwordConfirmation: 'any password'
+      }
+    }
+    const { name, email, password } = httpRequest.body
+    expect(addAccountSpy).toHaveBeenCalledWith({ name, email, password })
   })
 })
