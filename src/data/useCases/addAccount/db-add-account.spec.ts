a9d7e8b128080bfc9e3be2b88ea9efe47b88dba1
Bruno Costa <b.brunocosta@outlook.com>
2021-07-19 18:43:06 -0300

    test: ensure DbAccountAdapter throws if Encrypter throws

--- src/data/useCases/addAccount/db-add-account.spec.ts
+++ src/data/useCases/addAccount/db-add-account.spec.ts
@@ -6,17 +6,21 @@ describe('DbAddAccount UseCase', () => {
       return await new Promise(resolve => resolve('hashed password'))
     }
   }
-  const encrypterStub = new EncrypterStub()
-  const sut = new DbAddAccount(encrypterStub)
   const accountData = {
     name: 'valid name',
     email: 'valid email',
     password: 'valid password'
   }
-
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
   test('should call encripter with correct password', async () => {
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith(accountData.password)
+  })
+  test('Should throw if Encrypter throws', async () => {
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
   })
 })
