6cb6f2d569d88552a7f752b9c240daa515f68c3e
Bruno Costa <b.brunocosta@outlook.com>
2021-07-19 23:28:12 -0300

    feat: add exports

---
+++ src/data/useCases/addAccount/db-add-account.protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/models/account'
+export * from '../../../domain/useCases/addAccount.usecase'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/addAccountRepository'
