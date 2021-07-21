91d4fa053197d8824507433fb0b5aac6819b67dd
Bruno Costa <b.brunocosta@outlook.com>
2021-07-21 16:45:52 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/useCases/addAccount/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt.adapter'
+import { MongoAccountRepository } from '../../infra/db/mongodb/account/account.repository'
+import { SignUpController } from '../../presentation/controllers/signUp/signUp.controller'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidator = new EmailValidatorAdapter()
+  const mongoAccountRepository = new MongoAccountRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, mongoAccountRepository)
+  return new SignUpController(emailValidator, dbAddAccount)
+}
