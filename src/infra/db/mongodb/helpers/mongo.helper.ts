9cbf0ae9a7009a883eaefb06ab9139db1b5ef15c
Bruno Costa <b.brunocosta@outlook.com>
2021-07-21 16:36:56 -0300

    feat: connect to mongodb before app.listen

--- src/infra/db/mongodb/helpers/mongo.helper.ts
+++ src/infra/db/mongodb/helpers/mongo.helper.ts
@@ -2,7 +2,7 @@ import { MongoClient, Collection } from 'mongodb'
 export const MongoHelper = {
   client: null as unknown as MongoClient,
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL ?? ' ', {
+    this.client = await MongoClient.connect(uri, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     })
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
-
-app.listen(5050, () => console.log('server running on port 5050'))
+import env from '../main/config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo.helper'
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  console.log('db is connected')
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => {
+    console.log('server running on port 5050')
+  })
+}).catch(console.log)
