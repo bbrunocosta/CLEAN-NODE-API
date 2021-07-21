fa9661e5f45d865f81afb101f904fa1b23ec65ba
Bruno Costa <b.brunocosta@outlook.com>
2021-07-21 14:14:18 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '../middleares/bodyparser'
+import { cors } from '../middleares/cors'
 import { Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
