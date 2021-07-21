b1cd7b9e3392ccaa135b045a43afaf064df43541
Bruno Costa <b.brunocosta@outlook.com>
2021-07-21 14:35:44 -0300

    test: ensure json is default content-type

---
+++ src/main/middleares/contentType.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-Type Middleware', () => {
+  test('Sould return ContentType as json as default', async () => {
+    app.get('/test_content-type-json', (req, res) => { res.send('') })
+    await request(app)
+      .get('/test_content-type-json')
+      .expect('content-type', /json/)
+  })
+  test('Sould return xml content type when forced', async () => {
+    app.get('/test_content-type-xml', (req, res) => { 
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content-type-xml')
+      .expect('content-type', /xml/)
+  })
+})
