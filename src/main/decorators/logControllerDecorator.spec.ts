ec52c1759114e6763bea3c355884323d04ba5dd6
Bruno Costa <b.brunocosta@outlook.com>
2021-07-21 21:41:27 -0300

    test: ensure LogControllerDecorator calls logErrorRepository.log with correct error stack

--- src/main/decorators/logControllerDecorator.spec.ts
+++ src/main/decorators/logControllerDecorator.spec.ts
@@ -1,4 +1,4 @@
-import { created } from '../../presentation/helpers/http.helper'
+import { created, serverError } from '../../presentation/helpers/http.helper'
 import { HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './logControllerDecorator'
 
@@ -8,8 +8,16 @@ describe('Log Controller Decorator', () => {
       return created({ res: 'ControllerStubResponses' })
     }
   }
+  class LogErrorRepositoryStub {
+    async log (stack: string): Promise<void> {
+
+    }
+  }
+  const fakeError = new Error()
+  fakeError.stack = 'any Stack'
+  const logErrorRepositoryStub = new LogErrorRepositoryStub()
   const controllerSub = new ControllerStub()
-  const sut = new LogControllerDecorator(controllerSub)
+  const sut = new LogControllerDecorator(controllerSub, logErrorRepositoryStub)
   test('Should call controller.handle', async () => {
     const handleSpy = jest.spyOn(controllerSub, 'handle')
     const httpRequest = {
@@ -33,5 +41,16 @@ describe('Log Controller Decorator', () => {
       status: 201,
       body: { res: 'ControllerStubResponses' }
     })
+  })
+  test('Sould call LogErrorRepository with correct error if controller returns a server error', async () => {
+    jest.spyOn(controllerSub, 'handle').mockReturnValueOnce(Promise.resolve(serverError(fakeError)))
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+    const httpRequest = {
+      body: {
+        name: 'bruno'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(logSpy).toHaveBeenCalledWith('any Stack')
   })
 })
